bc108072cd7624dcd5df2e1c908f695053565bb1
Geoffroy <nycgdf@gmail.com>
2017-10-23 10:59:19 +0100

    bucket env name

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import tinys3
 
-s3_bucket_name = 'gdf-artificial-intelligence'
+s3_bucket_name = os.environ['S3_BUCKET_NAME']
 S3_ACCESS_KEY = os.environ['S3_ACCESS_KEY']
 S3_SECRET_KEY = os.environ['S3_SECRET_KEY']
-conn = tinys3.Connection(S3_ACCESS_KEY, S3_SECRET_KEY)
+conn = tinys3.Connection(S3_ACCESS_KEY, S3_SECRET_KEY, tls=True)
