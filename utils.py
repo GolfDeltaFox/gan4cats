e41ef5771d8ccccf76a862e2018c0349cd84b12d
Geoffroy <nycgdf@gmail.com>
2017-10-24 11:38:36 +0100

    upload in a thread

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import config
 from config import conn, s3_bucket_name
 import tarfile
 from os import listdir, path
+import threading
+
+def upload_s3(conn, file_name, s3_bucket_name):
+    with open(file_name,'rb') as f:
+        conn.upload(file_name, f, s3_bucket_name)
 
 def save_on_s3(model_name, save_path):
     if s3_bucket_name:
@@ -11,5 +16,7 @@ def save_on_s3(model_name, save_path):
             out.add(path.join(save_path, afile))
         out.close()
         print('Uploading...')
-        with open(model_name+'.tar.gz','rb') as f:
-            conn.upload(model_name+'.tar.gz', f, s3_bucket_name)
+        # with open(model_name+'.tar.gz','rb') as f:
+            # conn.upload(model_name+'.tar.gz', f, s3_bucket_name)
+        thread = threading.Thread(target=upload_s3, args=(conn, model_name+'.tar.gz', s3_bucket_name))
+        thread.start()
